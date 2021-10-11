106899fd05ed9ea10fe0a20c6a2c2b190cdc5eb6
jac <me@jorge.red>
2021-10-11 21:29:44 +0200

    cambios

---
+++ lanza.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+freq_actual="freq_actual.csv"
+i=0
+ficheros=()
+for fichero in *.csv 
+do
+	i=$(($i + 1))
+	echo -e "\t$i) $fichero"
+	fichero+=("$fichero")
+done
+
+for var in "${ficheros[@]}"
+do
+	  echo "Hola ${var}"
+	    # do something on $var
+done
+
+echo -en "Selecciona una frecuencia [1-$i] "
+read eleccion
+elegido=${fichero[$eleccion]}
+if [[ -r $elegido ]]
+then
+	rm $freq_actual
+	ln -s $elegido $freq_actual
+	ls -o $freq_actual
+fi
+
+python gqrx_scan.py
