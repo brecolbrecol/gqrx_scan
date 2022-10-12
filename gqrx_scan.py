2b51bfa5bd516bca427365c9889791fcdfd7805f
jac <me@jorge.red>
2022-10-12 12:07:01 +0200

    cambios

--- gqrx_scan.py
+++ gqrx_scan.py
@@ -17,12 +17,12 @@ class Scanner:
 		"""
 		update the frequency/mode GQRX is listening to
 		"""
-                try:
-    		    tn = telnetlib.Telnet(self.host, self.port)
-                except Exception as e: 
-                    print("Error connecting to " + self.host + ":" + str(self.port) + "\n\t" + str(e))
-                    exit()
-    		tn.write(('%s\n' % msg).encode('ascii'))
+		try:
+			tn = telnetlib.Telnet(self.host, self.port)
+		except Exception as e:
+			print("Error connecting to " + self.host + ":" + str(self.port) + "\n\t" + str(e))
+			exit()
+		tn.write(('%s\n' % msg).encode('ascii'))
 		response = tn.read_some().decode('ascii').strip()
 		tn.write('c\n'.encode('ascii'))
 		return response
@@ -37,15 +37,16 @@ class Scanner:
 				self._set_freq(freq)
 				self._set_mode(self.freqs[freq]['mode'])
 				self._set_squelch(self.signalStrength)
-                                timenow = str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min)
-                                sys.stdout.write(timenow + " " + str(freq) + " " + str(self.freqs[freq]['tag']) + "\t\t" + str(self._get_level()) + "/" + str(self.signalStrength) + " dB\r")
-                                sys.stdout.flush()
-				time.sleep(0.2)
+				timenow = str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min)
+				sys.stdout.write(timenow + " " + str(freq) + " " + str(self.freqs[freq]['tag']) + "\t\t" + str(self._get_level()) + "/" + str(self.signalStrength) + " dB\r")
+				sys.stdout.flush()
+				time.sleep(0.3)
 				if float(self._get_level()) >= self.signalStrength:
-					while float(self._get_level()) >= self.signalStrength:
-                                            timenow = str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min)
-                                            print timenow + " " + str(freq) + " " + str(self.freqs[freq]['tag']) +  "\t" + str(self._get_level()) + "/" + str(self.signalStrength) + " dB -- esperando " + str(self.waitTime) + "s ..."
-					    time.sleep(self.waitTime)
+					time.sleep(0.5)
+				while float(self._get_level()) >= self.signalStrength:
+					timenow = str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min)
+					print timenow + " " + str(freq) + " " + str(self.freqs[freq]['tag']) +  "\t" + str(self._get_level()) + "/" + str(self.signalStrength) + " dB -- esperando " + str(self.waitTime) + "s ..."
+					time.sleep(self.waitTime)
 
 	def scan_range(self, minfreq, maxfreq, mode, step=500, save = None):
 		"""
@@ -94,34 +95,34 @@ class Scanner:
 
 		pass
 
-   	def load(self, freq_csv='freq_actual.csv'):
-   		"""
+	def load(self, freq_csv='freq_actual.csv'):
+		"""
    		read the csv file with the frequencies & modes
 		in it into a dict{} where keys are the freq and
 		the value is a dict with the mode and a tag
    		"""
-   		self.freqs = {}
-   		with open(freq_csv, 'r') as csvfile:
-   			reader = csv.reader(csvfile, delimiter = ',')
-   			for row in reader:
-   				freq = str(float(row[0])*1e5)	    					# 1e5 isn't good
-   				freq = int(freq.replace('.', ''))   					# converted to hz
+		self.freqs = {}
+		with open(freq_csv, 'r') as csvfile:
+			reader = csv.reader(csvfile, delimiter = ',')
+			for row in reader:
+				freq = str(float(row[0])*1e5)	    					# 1e5 isn't good
+				freq = int(freq.replace('.', ''))   					# converted to hz
 				if len(row) == 2:
 					self.freqs[freq] = {'mode': row[1], 'tag': None}
 				elif len(row) == 3:
-   					self.freqs[freq] = {'mode' : row[1], 'tag': row[2]}     # add the freq to the dict as a key and the mode as the value
+					self.freqs[freq] = {'mode' : row[1], 'tag': row[2]}     # add the freq to the dict as a key and the mode as the value
 
-   	def _set_freq(self, freq):
-   		return self._update("F %s" % freq)
+	def _set_freq(self, freq):
+		return self._update("F %s" % freq)
 
-   	def _set_mode(self, mode):
-   		return self._update("M %s" % mode)
+	def _set_mode(self, mode):
+		return self._update("M %s" % mode)
 
 	def _set_squelch(self, sql):
 		return self._update("L SQL %s" % sql)
 
-   	def _get_level(self):
-   		return self._update("l")
+	def _get_level(self):
+		return self._update("l")
 
 	def _get_mode(self):
 		return self._update('m')
